afbb90f7545d607113f243eaa1f36a16d0136df8
Radoslav Georgiev <radorado@hacksoft.io>
2020-06-19 10:50:10 +0300

    Restructure initial Django project to a structure we use
    - `config/`, `project_name/` and `manage.py` on top-level

--- config/settings/base.py
+++ config/settings/base.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'styleguide_example.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'styleguide_example.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.base')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
