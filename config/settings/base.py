598f39e9d786b1e80b106f4145eb8ca4553a4915
Radoslav Georgiev <radorado@hacksoft.io>
2020-06-19 13:30:06 +0300

    Add `users` app & `BaseUser`

--- config/settings/base.py
+++ config/settings/base.py
@@ -32,6 +32,10 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+LOCAL_APPS = [
+    'styleguide_example.users.apps.UsersConfig'
+]
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -39,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    *LOCAL_APPS
 ]
 
 MIDDLEWARE = [
@@ -98,6 +103,8 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+AUTH_USER_MODEL = 'users.BaseUser'
 
 
 # Internationalization
---
+++ styleguide_example/users/models.py
@@ -0,0 +1,66 @@
+from django.db import models
+from django.contrib.auth.models import (
+    BaseUserManager as BUM,
+    PermissionsMixin,
+    AbstractBaseUser
+)
+
+
+# Taken from here:
+# https://docs.djangoproject.com/en/3.0/topics/auth/customizing/#a-full-example
+# With some modifications
+
+
+class BaseUserManager(BUM):
+    def create_user(self, email, is_active=True, is_admin=False, password=None):
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            is_active=is_active,
+            is_admin=is_admin
+        )
+
+        if password is not None:
+            user.set_password(password)
+        else:
+            user.set_unusable_password()
+
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password=None):
+        user = self.create_user(
+            email=email,
+            is_active=True,
+            is_admin=True,
+            password=password,
+        )
+
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
+
+class BaseUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(
+        verbose_name='email address',
+        max_length=255,
+        unique=True,
+    )
+
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    objects = BaseUserManager()
+
+    USERNAME_FIELD = 'email'
+
+    def __str__(self):
+        return self.email
+
+    def is_staff(self):
+        return self.is_admin
