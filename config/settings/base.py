11accaaee1e2ca466a3fea8c4f368b940cfb2520
Radoslav Georgiev <radorado@hacksoft.io>
2020-06-19 14:26:23 +0300

    Add GitHub Actions support

--- config/settings/base.py
+++ config/settings/base.py
@@ -85,6 +85,18 @@ DATABASES = {
 }
 DATABASES['default']['ATOMIC_REQUESTS'] = True
 
+if os.environ.get('GITHUB_WORKFLOW'):
+    DATABASES = {
+        'default': {
+           'ENGINE': 'django.db.backends.postgresql',
+           'NAME': 'github_actions',
+           'USER': 'postgres',
+           'PASSWORD': 'postgres',
+           'HOST': '127.0.0.1',
+           'PORT': '5432',
+        }
+    }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
