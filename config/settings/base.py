66b94e1e347985cdb3696432c7529ea07a6125ec
Radoslav Georgiev <radorado@hacksoft.io>
2020-07-05 20:51:02 +0300

    Add some JWT related settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+from datetime import timedelta
+
 from .env_reader import env
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -142,3 +144,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=30),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=31),
+    # TODO: Open issue for having a callable with a user here
+    'SIGNING_KEY': env('DJANGO_JWT_SIGNING_KEY', default=SECRET_KEY)
+    # TODO: https://github.com/SimpleJWT/django-rest-framework-simplejwt/pull/157/files
+    # Add settings for http support
+}
