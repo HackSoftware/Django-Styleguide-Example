5e0fc1a82438a43d3b04a5c99d79846f2389af70
Radoslav Georgiev <radorado@hacksoft.io>
2020-07-08 21:54:00 +0300

    Add example with nested serializers

--- styleguide_example/common/apis.py
+++ styleguide_example/common/apis.py
@@ -2,12 +2,23 @@ from django.core.exceptions import ValidationError
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import serializers
 
 from styleguide_example.api.mixins import ApiErrorsMixin
 
 
 def trigger_error():
     raise ValidationError('Error from service')
+
+
+class NestedSerializer(serializers.Serializer):
+    bar = serializers.IntegerField()
+
+
+class TopLevelSerializer(serializers.Serializer):
+    foo = NestedSerializer()
+
+    array = serializers.ListField(child=serializers.IntegerField())
 
 
 class TriggerErrorApi(ApiErrorsMixin, APIView):
@@ -17,6 +28,18 @@ class TriggerErrorApi(ApiErrorsMixin, APIView):
     Instead of 500 Server Error
     """
     def get(self, request):
-        trigger_error()
+        serializer = TopLevelSerializer(
+            data={
+                'foo': {
+                    'bar': 'xyz'
+                },
+                'array': [
+                    1, 2, {}
+                ]
+            }
+        )
+        serializer.is_valid(raise_exception=True)
 
-        return Response()
+        # trigger_error()
+
+        return Response(serializer.validated_data)
