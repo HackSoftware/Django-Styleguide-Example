c4824e99869e7927e7219b568816b2324609425c
Radoslav Georgiev <radorado@hacksoft.io>
2020-07-08 10:45:46 +0300

    Add example service that triggers an error

--- styleguide_example/common/apis.py
+++ styleguide_example/common/apis.py
@@ -1,9 +1,22 @@
+from django.core.exceptions import ValidationError
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
 from styleguide_example.api.mixins import ApiErrorsMixin
 
 
+def trigger_error():
+    raise ValidationError('Error from service')
+
+
 class TriggerErrorApi(ApiErrorsMixin, APIView):
+    """
+    Since we have ApiErrorsMixin,
+    The API will fail with 400 Bad Request,
+    Instead of 500 Server Error
+    """
     def get(self, request):
+        trigger_error()
+
         return Response()
