f78950bc191b2023c2ad91948afa46e35242f574
Radoslav Georgiev <radorado@hacksoft.io>
2021-04-01 15:55:46 +0300

    Extract logic to `SessionAsHeaderAuthentication`

--- styleguide_example/api/mixins.py
+++ styleguide_example/api/mixins.py
@@ -7,7 +7,7 @@ from django.contrib import auth
 
 from rest_framework import exceptions as rest_exceptions
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.authentication import SessionAuthentication
+from rest_framework.authentication import SessionAuthentication, BaseAuthentication
 
 from styleguide_example.api.errors import get_error_message
 
@@ -23,6 +23,36 @@ def get_auth_header(headers):
     return auth_type, auth_value
 
 
+class SessionAsHeaderAuthentication(BaseAuthentication):
+    """
+    In case we are dealing with issues like Safari not supporting SameSite=None,
+    And the client passes the session as Authorization header:
+
+    Authorization: Session 7wvz4sxcp3chm9quyw015n6ryre29b3u
+
+    Run the standard Django auth & try obtaining user.
+    """
+    def authenticate(self, request):
+        auth_header = get_auth_header(request.headers)
+
+        if auth_header is None:
+            return None
+
+        auth_type, auth_value = auth_header
+
+        if auth_type != 'Session':
+            return None
+
+        engine = import_module(settings.SESSION_ENGINE)
+        SessionStore = engine.SessionStore
+        session_key = auth_value
+
+        request.session = SessionStore(session_key)
+        user = auth.get_user(request)
+
+        return user, None
+
+
 class CsrfExemptedSessionAuthentication(SessionAuthentication):
     """
     DRF SessionAuthentication is enforcing CSRF, which may be problematic.
@@ -31,34 +61,9 @@ class CsrfExemptedSessionAuthentication(SessionAuthentication):
     def enforce_csrf(self, request):
         return
 
-    def authenticate(self, request):
-        auth_result = super().authenticate(request)
-
-        if auth_result is None:
-            auth_header = get_auth_header(request.headers)
-
-            if auth_header is None:
-                return auth_result
-
-            auth_type, auth_value = auth_header
-
-            if auth_type != 'Session':
-                return auth_result
-
-            engine = import_module(settings.SESSION_ENGINE)
-            SessionStore = engine.SessionStore
-            session_key = auth_value
-
-            request.session = SessionStore(session_key)
-            user = auth.get_user(request)
-
-            return user, None
-
-        return auth_result
-
 
 class ApiAuthMixin:
-    authentication_classes = (CsrfExemptedSessionAuthentication, )
+    authentication_classes = (CsrfExemptedSessionAuthentication, SessionAsHeaderAuthentication)
     permission_classes = (IsAuthenticated, )
 
 
