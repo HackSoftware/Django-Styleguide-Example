946c9b8ece2b2cea8f3eac0a22b2a4c6edbe9449
Radoslav Georgiev <radorado@hacksoft.io>
2021-04-06 11:06:09 +0300

    Add `get_object` util

--- styleguide_example/common/utils.py
+++ styleguide_example/common/utils.py
@@ -1,6 +1,21 @@
+from django.shortcuts import get_object_or_404
+from django.http import Http404
+
+
 def get_first_matching_attr(obj, *attrs, default=None):
     for attr in attrs:
         if hasattr(obj, attr):
             return getattr(obj, attr)
 
     return default
+
+
+def get_object(model_or_queryset, **kwargs):
+    """
+    Reuse get_object_or_404 since the implementation supports both Model && queryset.
+    Catch Http404 & return None
+    """
+    try:
+        return get_object_or_404(model_or_queryset, **kwargs)
+    except Http404:
+        return None
