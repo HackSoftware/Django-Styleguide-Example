5567dda96e3ea0e124e9eb54e187a05432a60489
Radoslav Georgiev <radorado@hacksoft.io>
2021-08-13 15:43:01 +0300

    Migrate `inline_serializer` from the styleguide

--- styleguide_example/common/utils.py
+++ styleguide_example/common/utils.py
@@ -1,3 +1,5 @@
+from rest_framework import serializers
+
 from django.shortcuts import get_object_or_404
 from django.http import Http404
 
@@ -19,3 +21,16 @@ def get_object(model_or_queryset, **kwargs):
         return get_object_or_404(model_or_queryset, **kwargs)
     except Http404:
         return None
+
+
+def create_serializer_class(name, fields):
+    return type(name, (serializers.Serializer, ), fields)
+
+
+def inline_serializer(*, fields, data=None, **kwargs):
+    serializer_class = create_serializer_class(name='', fields=fields)
+
+    if data is not None:
+        return serializer_class(data=data, **kwargs)
+
+    return serializer_class(**kwargs)
