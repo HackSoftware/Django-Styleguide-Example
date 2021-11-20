a8c4d8bd5360c46084f96c3626c13066d6217d3d
Radoslav Georgiev <radorado@hacksoft.io>
2021-11-20 18:38:58 +0200

    Trigger all errors in a smart way

--- styleguide_example/errors/apis.py
+++ styleguide_example/errors/apis.py
@@ -1,11 +1,11 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-from styleguide_example.errors.services import trigger_error
+from styleguide_example.errors.services import trigger_errors
 
 
 class TriggerErrorApi(APIView):
     def get(self, request):
-        trigger_error()
+        result = trigger_errors()
 
-        return Response()
+        return Response(result)
--- styleguide_example/errors/services.py
+++ styleguide_example/errors/services.py
@@ -1,6 +1,8 @@
+import inspect
+import sys
+
 from django.core.exceptions import (
     ValidationError as DjangoValidationError,
-    ObjectDoesNotExist,
     PermissionDenied
 )
 from django.http import Http404
@@ -9,6 +11,8 @@ from rest_framework import serializers, exceptions
 from rest_framework.exceptions import ValidationError as RestValidationError
 
 from styleguide_example.users.models import BaseUser
+
+from styleguide_example.api.errors import drf_default_with_modifications_exception_handler
 
 
 class NestedSerializer(serializers.Serializer):
@@ -22,52 +26,88 @@ class PlainSerializer(serializers.Serializer):
     nested = NestedSerializer()
 
 
-class ApplicationError(Exception):
-    def __init__(self, message, extra=None):
-        super().__init__(message)
-
-        self.message = message
-        self.extra = extra
+def trigger_django_validation():
+    raise DjangoValidationError("Some error message")
 
 
-def trigger_error():
-    # raise DjangoValidationError("Some error message")
+def trigger_django_permission_denied():
+    raise PermissionDenied()
 
-    # raise RestValidationError("Some error message")
-    # raise RestValidationError(detail={"error": "Some error message"})
 
-    # raise ObjectDoesNotExist()
-    # raise PermissionDenied()
+def trigger_django_404():
     raise Http404()
 
-    # serializer = PlainSerializer(data={
-    #     "email": "foo",
-    #     "nested": {}
-    # })
-    # serializer.is_valid(raise_exception=True)
 
-    # try:
-    #     serializer.is_valid(raise_exception=True)
-    # except RestValidationError as exc:
-    #     breakpoint()
-    #     raise
-
-    # raise ApplicationError(
-    #     message="You cannot do that operation",
-    #     extra={
-    #         "type": "RANDOM"
-    #     }
-    # )
-    # raise exceptions.Throttled()
-    # raise exceptions.UnsupportedMediaType(media_type="shano/manqk")
-    # raise exceptions.NotAcceptable()
-    # raise exceptions.MethodNotAllowed(method="SHANO")
-    # raise exceptions.NotFound()
-    # raise exceptions.PermissionDenied()
-    # raise exceptions.NotAuthenticated()
-    # raise exceptions.AuthenticationFailed()
-    # raise exceptions.ParseError()
-    # raise exceptions.ValidationError(set([1, 2, 3]))
-
+def trigger_model_clean():
     user = BaseUser()
     user.full_clean()
+
+
+def trigger_rest_validation_plain():
+    raise RestValidationError("Some error message")
+
+
+def trigger_rest_validation_detail():
+    raise RestValidationError(detail={"error": "Some error message"})
+
+
+def trigger_serialization_validation():
+    serializer = PlainSerializer(data={
+        "email": "foo",
+        "nested": {}
+    })
+    serializer.is_valid(raise_exception=True)
+
+
+def trigger_rest_throttled():
+    raise exceptions.Throttled()
+
+
+def trigger_rest_unsupported_media_type():
+    raise exceptions.UnsupportedMediaType(media_type="a/b")
+
+
+def trigger_rest_not_acceptable():
+    raise exceptions.NotAcceptable()
+
+
+def trigger_rest_method_not_allowed():
+    raise exceptions.MethodNotAllowed(method="POST")
+
+
+def trigger_rest_not_found():
+    raise exceptions.NotFound()
+
+
+def trigger_rest_permission_denied():
+    raise exceptions.PermissionDenied()
+
+
+def trigger_rest_not_authenticated():
+    raise exceptions.NotAuthenticated()
+
+
+def trigger_rest_authentication_failed():
+    raise exceptions.AuthenticationFailed()
+
+
+def trigger_rest_parse_error():
+    raise exceptions.ParseError()
+
+
+def trigger_errors():
+    result = {}
+
+    for name, member in inspect.getmembers(sys.modules[__name__]):
+        if inspect.isfunction(member) and name.startswith("trigger") and name != "trigger_errors":
+            try:
+                member()
+            except Exception as exc:
+                response = drf_default_with_modifications_exception_handler(exc, {})
+
+                if response is None:
+                    raise
+
+                result[name] = response.data
+
+    return result
