aa9c078ac7a8d659c18b371fc868048a40e325d8
Radoslav Georgiev <radorado@hacksoft.io>
2021-11-22 12:14:12 +0200

    Fix settings path for Celery

--- styleguide_example/tasks/celery.py
+++ styleguide_example/tasks/celery.py
@@ -5,7 +5,7 @@ import os
 from celery import Celery
 
 # set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.base')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.django.base')
 
 app = Celery('styleguide_example')
 
