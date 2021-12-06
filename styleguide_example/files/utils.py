31b9ea2c01023d58fef8b569dadbba1861ffd75a
kbadova <krasi@hacksoft.io>
2021-12-06 18:06:03 +0200

    Fix generating an upload url

--- styleguide_example/files/utils.py
+++ styleguide_example/files/utils.py
@@ -10,4 +10,4 @@ def file_generate_upload_path(instance, filename):
 
 
 def file_generate_local_upload_url(*, file_id: str):
-    return f"{settings.SERVER_HOST_DOMAIN}/api/files/images/{file_id}/local-upload/"
+    return f"{settings.SERVER_HOST_DOMAIN}/api/files/{file_id}/local-upload/"
