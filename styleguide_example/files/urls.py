c4149f9a9fe1001869764a6702bb4ccdeb0623a4
kbadova <krasi@hacksoft.io>
2021-12-06 11:52:18 +0200

    Drop duplication of "/files" in files urls

--- styleguide_example/files/urls.py
+++ styleguide_example/files/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from styleguide_example.files.apis import FileGeneratePrivatePresignedPostApi, FileLocalUploadAPI
 
 urlpatterns = [
-    path("files/private-presigned-post/", FileGeneratePrivatePresignedPostApi.as_view()),
+    path("private-presigned-post/", FileGeneratePrivatePresignedPostApi.as_view()),
     path(
-        "files/<uuid:file_id>/local-upload/",
+        "<uuid:file_id>/local-upload/",
         FileLocalUploadAPI.as_view(),
     ),
 ]
