c42ef6e454d9281a3ad014a0ed38827b3b195cc0
kbadova <krasi@hacksoft.io>
2021-12-06 18:09:44 +0200

    Make sure uploaded files can be served by the BE

--- styleguide_example/files/admin.py
+++ styleguide_example/files/admin.py
@@ -5,6 +5,6 @@ from styleguide_example.files.models import File
 
 @admin.register(File)
 class FileAdmin(admin.ModelAdmin):
-    list_display = ["id", "file_name"]
+    list_display = ["id", "file_name", "url"]
 
     ordering = ["-created_at"]
