8f4ddf0d8ab3997371e36b7df36578ab7aaa3b70
Radoslav Georgiev <radorado@hacksoft.io>
2022-01-15 17:29:16 +0200

    Type-check `ApiAuthMixin`

--- styleguide_example/api/mixins.py
+++ styleguide_example/api/mixins.py
@@ -1,10 +1,12 @@
+from typing import Sequence, Type, TYPE_CHECKING
+
 from importlib import import_module
 
 from django.conf import settings
 
 from django.contrib import auth
 
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, BasePermission
 from rest_framework.authentication import SessionAuthentication, BaseAuthentication
 
 from rest_framework_jwt.authentication import JSONWebTokenAuthentication
@@ -60,10 +62,20 @@ class CsrfExemptedSessionAuthentication(SessionAuthentication):
         return
 
 
+if TYPE_CHECKING:
+    # This is going to be resolved in the stub library
+    # https://github.com/typeddjango/djangorestframework-stubs/
+    from rest_framework.permissions import _PermissionClass
+
+    PermissionClassesType = Sequence[_PermissionClass]
+else:
+    PermissionClassesType = Sequence[Type[BasePermission]]
+
+
 class ApiAuthMixin:
-    authentication_classes = (
+    authentication_classes: Sequence[Type[BaseAuthentication]] = [
         CsrfExemptedSessionAuthentication,
         SessionAsHeaderAuthentication,
         JSONWebTokenAuthentication
-    )
-    permission_classes = (IsAuthenticated, )
+    ]
+    permission_classes: PermissionClassesType = (IsAuthenticated, )
