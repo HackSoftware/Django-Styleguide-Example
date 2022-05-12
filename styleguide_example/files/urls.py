095856b755ad9ca98abeb1ba2459ffa5ab0e2071
Danislav Pometkov <pometkov.d@protonmail.com>
2022-05-12 11:28:32 +0300

    added check for local direct upload

--- styleguide_example/files/urls.py
+++ styleguide_example/files/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.urls import path, include
 
 from styleguide_example.files.apis import (
@@ -7,6 +8,18 @@ from styleguide_example.files.apis import (
     FileDirectUploadFinishApi,
     FileDirectUploadLocalApi,
 )
+from styleguide_example.files.enums import FileUploadStorage
+
+
+direct_upload_urls = [
+    path("start/", FileDirectUploadStartApi.as_view(), name="start"),
+    path("finish/", FileDirectUploadFinishApi.as_view(), name="finish"),
+]
+
+if settings.FILE_UPLOAD_STORAGE == FileUploadStorage.LOCAL:
+    direct_upload_urls.append(
+        path("local/<str:file_id>/", FileDirectUploadLocalApi.as_view(), name="local")
+    )
 
 
 urlpatterns = [
@@ -20,23 +33,7 @@ urlpatterns = [
             ),
             path(
                 "direct/",
-                include(([
-                    path(
-                        "start/",
-                        FileDirectUploadStartApi.as_view(),
-                        name="start"
-                    ),
-                    path(
-                        "finish/",
-                        FileDirectUploadFinishApi.as_view(),
-                        name="finish"
-                    ),
-                    path(
-                        "local/<str:file_id>/",
-                        FileDirectUploadLocalApi.as_view(),
-                        name="local"
-                    )
-                ], "direct"))
+                include((direct_upload_urls, "direct"))
             )
         ], "upload"))
     )
