e21397791fad6ad8872ef4faae77d84f6d1308d6
Radoslav Georgiev <radorado@hacksoft.io>
2022-05-17 09:43:04 +0300

    Add error handling to the email sending task

--- styleguide_example/emails/services.py
+++ styleguide_example/emails/services.py
@@ -1,7 +1,10 @@
+import random
+
 from django.db import transaction
 from django.db.models.query import QuerySet
 from django.core.mail import EmailMultiAlternatives
 from django.utils import timezone
+from django.conf import settings
 
 from styleguide_example.core.exceptions import ApplicationError
 
@@ -12,9 +15,30 @@ from styleguide_example.emails.tasks import email_send as email_send_task
 
 
 @transaction.atomic
+def email_failed(email: Email) -> Email:
+    if email.status != Email.Status.SENDING:
+        raise ApplicationError(f"Cannot fail non-sending emails. Current status is {email.status}")
+
+    email, _ = model_update(
+        instance=email,
+        fields=["status"],
+        data={
+            "status": Email.Status.FAILED
+        }
+    )
+    return email
+
+
+@transaction.atomic
 def email_send(email: Email) -> Email:
     if email.status != Email.Status.SENDING:
         raise ApplicationError(f"Cannot send non-ready emails. Current status is {email.status}")
+
+    if settings.EMAIL_SENDING_FAILURE_TRIGGER:
+        failure_dice = random.uniform(0, 1)
+
+        if failure_dice <= settings.EMAIL_SENDING_FAILURE_RATE:
+            raise ApplicationError("Email sending failure triggered.")
 
     subject = email.subject
     from_email = "styleguide-example@hacksoft.io"
@@ -45,7 +69,7 @@ def email_send_all(emails: QuerySet[Email]):
 
     We don't want to decorate with @transaction.atomic,
     since we are executing updates, 1 by 1, in a separate atomic block,
-    so we can trigger transaction.on_commit for each email.
+    so we can trigger transaction.on_commit for each email, separately.
     """
     for email in emails:
         with transaction.atomic():
--- styleguide_example/emails/tasks.py
+++ styleguide_example/emails/tasks.py
@@ -1,12 +1,30 @@
 from celery import shared_task
+from celery.utils.log import get_task_logger
 
 from styleguide_example.emails.models import Email
 
 
-@shared_task
-def email_send(email_id):
-    # TODO: Add error handling
+logger = get_task_logger(__name__)
+
+
+def _email_send_failure(self, exc, task_id, args, kwargs, einfo):
+    email_id = args[0]
+    email = Email.objects.get(id=email_id)
+
+    from styleguide_example.emails.services import email_failed
+
+    email_failed(email)
+
+
+@shared_task(bind=True, on_failure=_email_send_failure)
+def email_send(self, email_id):
     email = Email.objects.get(id=email_id)
 
     from styleguide_example.emails.services import email_send
-    email_send(email)
+
+    try:
+        email_send(email)
+    except Exception as exc:
+        # https://docs.celeryq.dev/en/stable/userguide/tasks.html#retrying
+        logger.warning(f"Exception occurred while sending email: {exc}")
+        self.retry(exc=exc, countdown=5)
