b7d0312a2dcf26f84d0802532ae05425570d0243
LinaSSavova <l.savova@hacksoft.io>
2022-07-21 12:05:49 +0300

    Initial direct upload test

--- styleguide_example/files/tests/flows/test_direct_upload.py
+++ styleguide_example/files/tests/flows/test_direct_upload.py
@@ -1,4 +1,7 @@
 from django.test import TestCase
+from django.urls import reverse
+
+from rest_framework.test import APIClient
 
 
 class DirectUploadApiTests(TestCase):
@@ -7,3 +10,15 @@ class DirectUploadApiTests(TestCase):
 
     1. A start-upload-finish cycle, where we patch the presign generation with local upload storage.
     """
+    def setUp(self):
+        self.client = APIClient()
+
+        self.direct_upload_start_url = reverse("api:files:upload:direct:start")
+        self.direct_upload_finish_url = reverse("api:files:upload:direct:finish")
+        self.direct_upload_local_url = lambda file: reverse(
+            "api:files:upload:direct:local",
+            kwargs={"file_id": str(file.id)}
+        )
+
+    def test_direct_upload(self):
+        pass
