e43f3108d0e447ddfd950fccf4b7b4fce2f84487
Radoslav Georgiev <radorado@hacksoft.io>
2022-08-11 10:20:25 +0300

    `inline_serializer`: Give the anonymous serializer a name

--- styleguide_example/api/utils.py
+++ styleguide_example/api/utils.py
@@ -6,7 +6,7 @@ def create_serializer_class(name, fields):
 
 
 def inline_serializer(*, fields, data=None, **kwargs):
-    serializer_class = create_serializer_class(name='', fields=fields)
+    serializer_class = create_serializer_class(name='inline_serializer', fields=fields)
 
     if data is not None:
         return serializer_class(data=data, **kwargs)
